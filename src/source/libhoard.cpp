1dded629d9e1dd7dc40a0dc472b38eac68ce8014
Rahul Mangalampalli <rahul_mangalampalli@yahoo.in>
2024-05-06 15:10:13 -0400

    Updated libhoard.cpp

--- src/source/libhoard.cpp
+++ src/source/libhoard.cpp
@@ -1,91 +1,48 @@
-/* -*- C++ -*- */
+/* -- C++ -- */
 
 /*
-
   The Hoard Multiprocessor Memory Allocator
   www.hoard.org
 
   Author: Emery Berger, http://www.emeryberger.com
   Copyright (c) 1998-2020 Emery Berger
 
-  See the LICENSE file at the top-level directory of this
-  distribution and at http://github.com/emeryberger/Hoard.
-
+  See the LICENSE file at the top-level directory of this distribution
+  and at http://github.com/emeryberger/Hoard.
 */
 
 /*
  * @file   libhoard.cpp
  * @brief  This file replaces malloc etc. in your application.
- * @author Emery Berger <http://www.emeryberger.com>
  */
 
 #include <cstddef>
 #include <new>
+#include <iostream>
 
 #include "VERSION.h"
-
-#define versionMessage "Using the Hoard memory allocator (http://www.hoard.org), version " HOARD_VERSION_STRING "\n"
-
-// Disable size checks in ANSIwrapper.
-#define HL_NO_MALLOC_SIZE_CHECKS 0
-
 #include "heaplayers.h"
-
-// The undef below ensures that any pthread_* calls get strong
-// linkage.  Otherwise, our versions here won't replace them.  It is
-// IMPERATIVE that this line appear before any files get included.
-
-#undef __GXX_WEAK__ 
+#include "hoardtlab.h"
 
 #if defined(_WIN32)
 #define WIN32_LEAN_AND_MEAN
-
-// Maximize the degree of inlining.
 #pragma inline_depth(255)
-
-// Turn inlining hints into requirements.
 #define inline __forceinline
-#pragma warning(disable:4273)
-#pragma warning(disable: 4098)  // Library conflict.
-#pragma warning(disable: 4355)  // 'this' used in base member initializer list.
-#pragma warning(disable: 4074)	// initializers put in compiler reserved area.
-#pragma warning(disable: 6326)  // comparison between constants.
-
+#pragma warning(disable: 4273 4098 4355 4074 6326) // Consolidate warning disables
 #endif
 
-#if HOARD_NO_LOCK_OPT
-// Disable lock optimization.
-volatile bool anyThreadCreated = true;
-#else
-// The normal case. See heaplayers/spinlock.h.
-volatile bool anyThreadCreated = false;
-#endif
-
-#include "hoardtlab.h"
-
-//
-// The base Hoard heap.
-//
-
+volatile bool anyThreadCreated = false;  // Default to no thread having been created
 
 /// Maintain a single instance of the main Hoard heap.
-
 Hoard::HoardHeapType * getMainHoardHeap() {
-  // This function is C++ magic that ensures that the heap is
-  // initialized before its first use. First, allocate a static buffer
-  // to hold the heap.
-
-  static double thBuf[sizeof(Hoard::HoardHeapType) / sizeof(double) + 1];
-
-  // Now initialize the heap into that buffer.
-  static auto * th = new (thBuf) Hoard::HoardHeapType;
-  return th;
+    static double thBuf[sizeof(Hoard::HoardHeapType) / sizeof(double) + 1];
+    static auto * th = new (thBuf) Hoard::HoardHeapType;
+    return th;
 }
 
 TheCustomHeapType * getCustomHeap();
 
-enum { MAX_LOCAL_BUFFER_SIZE = 256 * 131072 };
-static char initBuffer[MAX_LOCAL_BUFFER_SIZE];
+static char initBuffer[256 * 131072]; // 256 * 131072 = MAX_LOCAL_BUFFER_SIZE
 static char * initBufferPtr = initBuffer;
 
 extern bool isCustomHeapInitialized();
@@ -93,73 +50,50 @@ extern bool isCustomHeapInitialized();
 #include "Heap-Layers/wrappers/generic-memalign.cpp"
 
 extern "C" {
-
-#if defined(__GNUG__)
-  void * xxmalloc (size_t sz)
+#if defined(_GNUC_)
+#define FLATTEN _attribute_((flatten))
+#define ALLOC_SIZE _attribute_((alloc_size(1)))
+#define MALLOC _attribute_((malloc))
 #else
-  void * __attribute__((flatten)) xxmalloc (size_t sz) __attribute__((alloc_size(1))) __attribute((malloc))
+#define FLATTEN
+#define ALLOC_SIZE
+#define MALLOC
 #endif
-  {
-    if (isCustomHeapInitialized()) {
-      void * ptr = getCustomHeap()->malloc (sz);
-      if (ptr == nullptr) {
-	fprintf(stderr, "INTERNAL FAILURE.\n");
-	abort();
-      }
-      return ptr;
+
+    void * xxmalloc(size_t sz) FLATTEN ALLOC_SIZE MALLOC {
+        if (isCustomHeapInitialized()) {
+            void * ptr = getCustomHeap()->malloc(sz);
+            if (ptr == nullptr) {
+                std::cerr << "INTERNAL FAILURE.\n";
+                abort();
+            }
+            return ptr;
+        }
+        // Fallback to initial buffer if heap is not initialized
+        void * ptr = initBufferPtr;
+        initBufferPtr += sz;
+        if (initBufferPtr > initBuffer + sizeof(initBuffer)) {
+            abort();
+        }
+        return ptr;
     }
-    // We still haven't initialized the heap. Satisfy this memory
-    // request from the local buffer.
-    void * ptr = initBufferPtr;
-    initBufferPtr += sz;
-    if (initBufferPtr > initBuffer + MAX_LOCAL_BUFFER_SIZE) {
-      abort();
+
+    void xxfree(void * ptr) {
+        getCustomHeap()->free(ptr);
     }
-    {
-      static bool initialized = false;
-      if (!initialized) {
-	initialized = true;
-#if !defined(_WIN32)
-	/* fprintf(stderr, versionMessage); */
-#endif
-      }
+
+    void * xxmemalign(size_t alignment, size_t sz) {
+        return generic_xxmemalign(alignment, sz);
     }
-    return ptr;
-  }
 
-#if defined(__GNUG__)
-  void xxfree (void * ptr)
-#else
-  void xxfree (void * ptr)
-#endif
-  {
-    getCustomHeap()->free (ptr);
-  }
+    size_t xxmalloc_usable_size(void * ptr) {
+        return getCustomHeap()->getSize(ptr);
+    }
 
- 
-#if defined(__GNUG__)
-  void * xxmemalign (size_t alignment, size_t sz) {
-#else
-  void * xxmemalign (size_t alignment, size_t sz) {
-#endif
-    return generic_xxmemalign(alignment, sz);
-  }
-    
-  size_t xxmalloc_usable_size (void * ptr) {
-    return getCustomHeap()->getSize (ptr);
-  }
+    void xxmalloc_lock() {}
+    void xxmalloc_unlock() {}
+}
 
-  void xxmalloc_lock() {
-    // Undefined for Hoard.
-  }
-
-  void xxmalloc_unlock() {
-    // Undefined for Hoard.
-  }
-
-} // namespace Hoard
-
-#if defined(__linux__) && !defined(__MUSL__)
-// include gnuwrapper here to aid inlining of xxmalloc + friends
+#if defined(_linux) && !defined(MUSL_)
 #include "Heap-Layers/wrappers/gnuwrapper.cpp"
 #endif
